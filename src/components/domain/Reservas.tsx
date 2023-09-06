bd35dcbed919c7142a659b641703aba0eac76bba
Harysson <haryssonsoares@gmail.com>
2023-09-05 21:46:28 -0300

    feat: add dayjs to sabe dates

--- src/components/domain/Reservas.tsx
+++ src/components/domain/Reservas.tsx
@@ -1,13 +1,17 @@
+import dayjs from "dayjs";
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 export const Reservas = () => {
-  const [value, onChange] = useState<Value>(new Date());
+  const [value, onChange] = useState<Value>(dayjs().toDate());
   return (
     <div>
-      <Calendar onChange={onChange} value={value} />
+      <Calendar
+        onChange={(date) => console.log("date = ", date)}
+        value={[dayjs().toDate(), dayjs().add(1, "day").toDate()]}
+      />
     </div>
   );
 };
