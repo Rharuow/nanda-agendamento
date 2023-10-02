675c0f350288665bfeb2d6abd4045f5837d47241
Harysson <haryssonsoares@gmail.com>
2023-10-02 19:16:39 -0300

    feat: schedule ordered by date desc

--- src/service/schedules/list.ts
+++ src/service/schedules/list.ts
@@ -1,13 +1,18 @@
 import { getDocs } from "firebase/firestore";
 import { schedulesCollection } from "../collections";
 import { Schedule } from "..";
+import dayjs from "dayjs";
 
 export const listSchedules = async () => {
   try {
-    return (await getDocs(schedulesCollection)).docs.map((schedule) => ({
-      ...schedule.data(),
-      id: schedule.id,
-    })) as Array<Schedule>;
+    return (
+      (await getDocs(schedulesCollection)).docs.map((schedule) => ({
+        ...schedule.data(),
+        id: schedule.id,
+      })) as Array<Schedule>
+    ).sort((current, next) => {
+      return dayjs(current.date).isBefore(dayjs(next.date)) ? 0 : -1;
+    });
   } catch (error) {
     console.log("listSchedule = ", error);
     return false;
