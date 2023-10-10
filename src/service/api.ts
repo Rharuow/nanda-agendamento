e205745b06133089fa4d05475fdf29580299ecfe
Harysson <haryssonsoares@gmail.com>
2023-10-10 18:55:58 -0300

    fix: get studente per schedule

--- src/service/api.ts
+++ src/service/api.ts
@@ -3,7 +3,6 @@ export {
   getStudent,
   listStudents,
   getStudentByName,
-  getStudentsPerSchedule,
 } from "./students";
 
 export { listSchedules, getSchedule, createScheduling } from "./schedules";
